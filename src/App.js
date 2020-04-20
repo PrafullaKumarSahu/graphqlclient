15ff3dfeff47c343f521bd46430e23d7c2df956c
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2020-04-21 02:32:01 +0530

    Add basic component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import BookList from './components/BookList'
 
 function App() {
   return (
     <div className="App" id="main">
-      <header className="App-header">GraphQL Books</header>
+      <header className="App-header">GraphQL Books</header> 
+      <BookList></BookList>
     </div>
   );
 }
